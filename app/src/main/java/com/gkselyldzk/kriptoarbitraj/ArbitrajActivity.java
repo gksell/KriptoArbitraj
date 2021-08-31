47efce14c6d34a349e3f9dfbbe657e86a2ce16b9
YILDIZAK <gokselyildizak@hotmail.com>
2021-08-31 12:38:17 +0300

    merhaba 12.37

--- app/src/main/java/com/gkselyldzk/kriptoarbitraj/ArbitrajActivity.java
+++ app/src/main/java/com/gkselyldzk/kriptoarbitraj/ArbitrajActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class ArbitrajActivity extends AppCompatActivity {
+    // Merhabalarrrr 12.37
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
